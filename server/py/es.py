7b96fd9bd8e5283f8856814358fed379c7635e09
steventheworker <steventheworker@gmail.com>
2022-08-14 04:16:11 -0700

    remove screenshots to stop crashes - Ubuntu 22.04 (Wayland)

--- server/py/es.py
+++ server/py/es.py
@@ -31,17 +31,6 @@ for _e in events:
 
 # force a refresh after simulating input (vvv) (Below code === screeny.py + (time.sleep(0.25)))
 if sys.argv[2] == 'y':
-    import base64
-    import mss
-    from PIL import Image
-    from io import BytesIO
+    from screeny import grab
     time.sleep(0.25)
-    with mss.mss() as sct:
-        monitor = sct.monitors[1]   # Use the 1st monitor
-        im = sct.grab(monitor)
-        img = Image.frombytes("RGB", im.size, im.bgra, "raw", "BGRX")
-        byteio = BytesIO()
-        img.save(byteio, format='JPEG', optimize=True, quality=20)
-        b64 = base64.b64encode(byteio.getvalue())
-        base64str = b64.decode('utf-8')
-        print(base64str)
+    print(grab())
--- server/py/screeny.py
+++ server/py/screeny.py
@@ -1,21 +1,34 @@
-# send a base64 screenshot  ===  aka the bottom of es.py
+# send a base64 screenshot w/o saving it to the drive
 import base64
-import mss
 from PIL import Image
 from io import BytesIO
+import platform
 
-with mss.mss() as sct:
-    monitor = sct.monitors[1]  # Use the 1st monitor
-    im = sct.grab(monitor)
-    # raw_bytes = mss.tools.to_png(im.rgb, im.size)
-    # make pil image
-    # img = Image.open(BytesIO(raw_bytes))
-    img = Image.frombytes("RGB", im.size, im.bgra, "raw", "BGRX")
-    byteio = BytesIO()
-    # convert to jpg
-    img.save(byteio, format='JPEG', optimize=True, quality=20)
-    # read bytes -> b64
-    b64 = base64.b64encode(byteio.getvalue())
-    base64str = b64.decode('utf-8')
-    print(base64str)
-    # print(len(base64str))
+def grab():
+    os_ = platform.system().lower()
+
+    # mss literally only doesn't work with Ubuntu 22.04 (a.k.a. "Wayland")
+    if os_ == "linux":
+        from os import environ
+        if 'wayland' in environ.get('WAYLAND_DISPLAY', ''):
+            return "data:image/jpeg;base64,"
+
+    import mss
+    with mss.mss() as sct:
+        monitor = sct.monitors[1]  # Use the 1st monitor
+        im = sct.grab(monitor)
+        # raw_bytes = mss.tools.to_png(im.rgb, im.size)
+        # make pil image
+        # img = Image.open(BytesIO(raw_bytes))
+        img = Image.frombytes("RGB", im.size, im.bgra, "raw", "BGRX")
+        byteio = BytesIO()
+        # convert to jpg
+        img.save(byteio, format='JPEG', optimize=True, quality=20)
+        # read bytes -> b64
+        b64 = base64.b64encode(byteio.getvalue())
+        base64str = b64.decode('utf-8')
+        # print(len(base64str))
+        return base64str
+
+if __name__ == "__main__":
+    print(grab())
